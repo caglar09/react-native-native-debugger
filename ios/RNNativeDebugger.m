75427d31266b8d9bc0517e10cb7b7ef6bde26deb
Çağlar Yıldırım <45654646+caglar09@users.noreply.github.com>
2026-09-18 11:24:44 +0300

    feat: expose iOS runtime telemetry controls

--- ios/RNNativeDebugger.m
+++ ios/RNNativeDebugger.m
@@ -1,5 +1,6 @@
 #import "RNNativeDebugger.h"
 #import "RNNativeDebuggerNativeSink.h"
+#import "RNNativeDebuggerRuntimeTelemetry.h"
 
 @implementation RNNativeDebugger
 
@@ -7,6 +8,18 @@ RCT_EXPORT_MODULE(RNNativeDebugger)
 
 + (BOOL)requiresMainQueueSetup {
   return NO;
+}
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    [RNNativeDebuggerRuntimeTelemetry startWithIntervalMs:1000.0];
+  }
+  return self;
+}
+
+- (void)dealloc {
+  [RNNativeDebuggerRuntimeTelemetry stop];
 }
 
 - (NSArray<NSString *> *)supportedEvents {
@@ -41,6 +54,20 @@ RCT_REMAP_METHOD(clearBufferedEvents,
 
 RCT_EXPORT_METHOD(setEnabled:(BOOL)enabled) {
   [RNNativeDebuggerNativeSink setEnabled:enabled];
+}
+
+RCT_EXPORT_METHOD(startRuntimeTelemetry:(double)intervalMs) {
+  [RNNativeDebuggerRuntimeTelemetry startWithIntervalMs:intervalMs];
+}
+
+RCT_EXPORT_METHOD(stopRuntimeTelemetry) {
+  [RNNativeDebuggerRuntimeTelemetry stop];
+}
+
+RCT_REMAP_METHOD(getRuntimeMetrics,
+                 getRuntimeMetricsWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject) {
+  resolve([RNNativeDebuggerRuntimeTelemetry snapshot]);
 }
 
 @end
